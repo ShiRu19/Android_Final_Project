2c40b4c19c6f781e7469612f1c10e9382d3749e0
keiunn <k20625178@gmail.com>
2022-06-17 20:24:40 +0800

    update(2022/06/17)
    Signed-off-by: keiunn <k20625178@gmail.com>

--- app/app/src/main/java/t4/sers/activity/AddFootprintsActivity.java
+++ app/app/src/main/java/t4/sers/activity/AddFootprintsActivity.java
@@ -1,13 +1,16 @@
 package t4.sers.activity;
 
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import java.sql.Time;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -15,13 +18,15 @@ import t4.sers.R;
 
 public class AddFootprintsActivity extends AppCompatActivity {
 
-    private EditText footprints_date;
+    private EditText footprints_date,footprints_time1,footprints_time2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.addfootprints);
         footprints_date = findViewById(R.id.pick_date);
+        footprints_time1 = findViewById(R.id.pick_time1);
+        footprints_time2 = findViewById(R.id.pick_time2);
     }
     public void showDatePicker(View view) {
         Calendar calendar = Calendar.getInstance();
@@ -39,7 +44,7 @@ public class AddFootprintsActivity extends AppCompatActivity {
                 dpv.dateVerification(AddFootprintsActivity.this, date_picker);
                 String date_string = dpv.getDateString(0);
 
-                if(date_string == getResources().getString(R.string.notification_dateHint)) {
+                if(date_string == getResources().getString(R.string.addfootprints_dateHint)) {
                     // 輸入不合法
                     footprints_date.setTextColor(getResources().getColor(R.color.red_500));
                     footprints_date.setText(date_string);   // 填入結果
@@ -51,6 +56,51 @@ public class AddFootprintsActivity extends AppCompatActivity {
                 }
             }
         }, year, month, day).show();
+    }
+    int h1,h2,m1,m2;
+    public void showTimePicker_start(View view) {
+        Calendar calendar = Calendar.getInstance();
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);      //取得現在的小時分鐘
+        int min = calendar.get(Calendar.MINUTE);
+        new TimePickerDialog(AddFootprintsActivity.this, new TimePickerDialog.OnTimeSetListener(){
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                h1=hourOfDay;
+                m1=minute;
+                String mTime1 = String.valueOf(hourOfDay) + " : " + String.valueOf(minute);
+                footprints_time1.setText(mTime1);// 填入結果
+            }
+
+        }, hour, min, true).show();
+    }
+    public void showTimePicker_end(View view) {
+        Calendar calendar = Calendar.getInstance();
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);      //取得現在的小時分鐘
+        int min = calendar.get(Calendar.MINUTE);
+        new TimePickerDialog(AddFootprintsActivity.this, new TimePickerDialog.OnTimeSetListener(){
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                h2=hourOfDay;
+                m2=minute;
+                String mTime2 = String.valueOf(hourOfDay) + " : " + String.valueOf(minute);
+                if(h1<h2){
+                    footprints_time2.setText(mTime2);// 填入結果
+                }
+                else if(h1==h2 & m1<m2){
+                    footprints_time2.setText(mTime2);// 填入結果
+                }
+                else{
+                    hint();
+                    mTime2 = "    "+" : ";
+                    footprints_time2.setText(mTime2);
+                }
+            }
+        }, hour, min, true).show();
+    }
+
+    public void hint(){
+        Toast toast = Toast.makeText(this,"請選擇正確的時間！",Toast.LENGTH_LONG);
+        toast.show();
     }
 
 }
